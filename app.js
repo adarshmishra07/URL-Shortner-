6aac5d91dc7ae1f260c577f9a9c6061035d7dce1
Adarsh Mishra <56035184+adarshmishra07@users.noreply.github.com>
2020-05-28 02:56:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,41 +4,69 @@ var request = require('request');
 
 var app = express();
 
-app.set('view engine','ejs');
+app.use('/public', express.static('public'));
+app.use(express.json({
+    limit: '1mb'
+}));
+app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }));
-app.get('/',(req,res)=>{
-    res.render('index',{short: '',msg:' '});
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        short: '',
+        msg: ' '
+    });
 })
 
-app.post('/',(req,res)=>{
-    var url= req.body.url;
-    var id = req.body.id;
+app.post('/', (req, res) => {
+    var error = '';
+    var msg = '';
+    var short = '';
+    var url = req.body.url;
+    var ids = req.body.ids;
     var data = {
         url,
-        id 
+        id: ids
     };
     const jsonstring = JSON.stringify(data);
-
-    request ({
+    request({
         url: "https://i1oqepnvgj.execute-api.us-east-1.amazonaws.com/prod/app",
         method: 'POST',
-        body : jsonstring,
-        headers : {
+        body: jsonstring,
+        headers: {
             'content-type': 'application/json'
         }
-    } ,function (error, response, body){
-        if(!error && response.statusCode == 200){
-          console.log(body);
-
-          res.render('index',{short:'https://i1oqepnvgj.execute-api.us-east-1.amazonaws.com/prod/'+ data.id,msg:''})
-         }
-         if(response.statusCode == 500){
-            res.render('index',{msg: "Please Use Different Alias Name",short:''})
-         }
-      })
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            short = 'https://i1oqepnvgj.execute-api.us-east-1.amazonaws.com/prod/' + data.id;
+            res.render('index', {
+                short,
+                msg
+            })
+        }
+        if (response.statusCode != 200) {
+            msg = "Please Use Different Alias Name";
+            res.render('index', {
+                msg,
+                short
+            })
+        }
     })
 
-    const PORT = process.env.PORT || 80;
+})
 
-    app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+app.get('*', function (req, res) {
+
+    res.status(404).redirect('public/404.html')
+
+});
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
